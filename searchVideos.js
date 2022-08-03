f9772b00552f2faa707c01986aab60d58f389291
JannisRex <rex.jannis@web.de>
2022-08-03 02:06:05 +0200

    [change] to get response from all pages and save to file(s). Page Number is hardcoded, because there is no way to do GET-request async. I have to do 1 request to see body.total in response TODO: promisify Vimeo Client
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- searchVideos.js
+++ searchVideos.js
@@ -1,40 +1,54 @@
 var config = require('./config.json')
-var util = require('util')
-const { request } = require('http')
 let Vimeo = require('vimeo').Vimeo
+
+const user = '/users/109239427'
+const pathDE = user + '/folders/3299245/videos?fields=uri' // Videos DE
+const pathEN = user + '/folders/3410229/videos?fields=uri' // Videos EN
+const path = user + '/videos?fields=uri'
+
+// Setting Auth + Token from config
+const client = new Vimeo(
+  config.client_id,
+  config.client_secret,
+  config.access_token
+)
+
+const basicQuery = {
+  // GET REQUEST for all Videos with an unrated content_rating param
+  method: 'GET',
+  path,
+  query: {
+    page: 1,
+    per_page: 100,
+    direction: 'asc',
+    filter: 'content_rating',
+    filter_content_rating: 'unrated',
+  },
+}
 
 // Queries Video ID's and writes them to result.txt
 searchVideos = () => {
-  // Setting Auth + Token from config
-  const client = new Vimeo(
-    config.client_id,
-    config.client_secret,
-    config.access_token
-  )
+  const totalResponses = 1627 // body.total in every response
+  // !!! This is hardcoded, need to do GET request async so I can fetch body.total before calling again !!! \\
+  // TODO: create promise helper for client.request or use callback
+  const pages = Math.floor(totalResponses / 100) + 1
 
-  const user = '/users/109239427'
-  const pathDE = user + '/folders/3299245/videos?fields=uri' // Videos DE
-  const pathEN = user + '/folders/3410229/videos?fields=uri' // Videos EN
-  const path = user + '/videos?fields=uri'
-
-  const query = {
-    // GET REQUEST for all Videos with an unrated content_rating param
-    method: 'GET',
-    path,
-    query: {
-      page: 1,
-      per_page: 5,
-      direction: 'asc',
-      filter: 'content_rating',
-      filter_content_rating: 'unrated',
-    },
+  for (let page = 1; page <= pages; page++) {
+    let query = createQuery(page)
+    searchByQuery(client, query, page)
   }
+}
 
-  searchByQuery(client, query)
+// Only adjusts page number for query
+// So we can get ALL pages at once and save to file(s)
+createQuery = (page) => {
+  let query = basicQuery
+  query.query.page = page
+  return query
 }
 
 // searches by given query
-searchByQuery = (client, query) => {
+searchByQuery = (client, query, page) => {
   // Make an API request
   client.request(query, async (error, body, statusCode, headers) => {
     if (error) {
@@ -44,7 +58,8 @@ searchByQuery = (client, query) => {
       const searchResponse = body.data
       const filteredResponse = filterID(searchResponse) // Filter for ID's only
       const formattedResponse = String(filteredResponse).replaceAll(',', ' ') // Replace Comma with Whitespace
-      writeToFile(formattedResponse, '/result.txt') // Writes content to File with given name
+      writeToFile(formattedResponse, './results/result' + page + '.txt') // Writes content to File with given name
+      // writeToFile(String(body.total), './results/total.txt') // Writes count of responses to file
     }
 
     console.log('status code: ', statusCode) // 200 or 400/404
